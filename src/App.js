33b7558c47fdaae84220b5f16080caf80b486cc4
anybody <dwiyual@gmail.com>
2021-09-24 21:58:16 -0500

    created a new react app for trotter floor cleaning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App() 
+{
+  return(
+  <nav id="nav" className="navbar">
+  <a href="www.google.com">Home</a>
+  <a href="www.google.com">About</a>
+  <a href="www.google.com">Services</a>
+  <a href="www.google.com">Our Work</a>
+  <a href="www.google.com">Contact</a>
+</nav>
   );
 }
 
