9f1c8807a3b237ca2a22d8730c5e654e0b6f04e2
dwiyual <dwiyual@gmail.com>
2022-01-21 15:02:48 -0600

    added p tag to About component

--- src/App.js
+++ src/App.js
@@ -35,6 +35,18 @@ export class About extends React.Component {
     return (
       <main>
         <h1>We Clean many types of comercial buildings.</h1>
+        <p>Trotter Floor Cleaning Service is a small, family owned bussiness out of Lincoln, NE. We have been
+                    doing buisness
+                    for over 50 years. Started in 1964 by Merlyn Trotter, originally doing only floors, we have since
+                    expanded
+                    our company to a full service cleaning company. We provide impeccable cleaning from experience
+                    and knowledgable personal. We can clean many different types of projects from residental to
+                    commercial
+                    buildings, we have the equipment, experience and knowledge to handle almost any job. All at a fair
+                    and
+                    competetive
+                    rate. Just contact us and we will be happy to give a quote.
+                </p>
         <nav>
           <Link to="/">Home</Link>
         </nav>
