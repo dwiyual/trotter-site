6ccc18cc0d6fa625bc231721fe8fa1bcd4506dee
dwiyual <dwiyual@gmail.com>
2022-01-21 12:32:31 -0600

    started the trotter site again over again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { About, Application, Home, Services } from './components';
-function App() {
 
+function App() {
   return (
-    <>
-     <Home/>
-     <About/>
-     <Services/>
-     <Application/>
-      </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 
 export default App;
