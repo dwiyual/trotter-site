8419516715ee529488a348518b9f94d95ac9c4f4
dwiyual <dwiyual@gmail.com>
2022-01-22 03:00:22 -0600

    added some styling and too app.css and got css to show up becuase i changed the class name to app but still need to get my other classes to show.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom"
-import './App.css';
-
+import "./App.css";
 export function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <h1>Welcome to Trotter Floor Cleaning!</h1>
+      <Link to = "/">Home</Link>
+      <Link to = "about">About</Link>
+      <Link to = "apply">Apply</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path ="apply" element={<Application/>}/>
       </Routes>
     </div>
   );
@@ -20,8 +23,8 @@ export class Home extends
     return (
       <>
         <main>
-          <h1>Welcome to Trotter Floor Cleaning</h1>
-          <nav>
+          <h1>Hello</h1>
+          <nav className="link">
             <Link to="/about">About</Link>
           </nav>
         </main>
@@ -33,27 +36,42 @@ export class Home extends
 export class About extends React.Component {
   render() {
     return (
-      <main>
-        <h1>We Clean many types of comercial buildings.</h1>
-        <p>Trotter Floor Cleaning Service is a small, family owned bussiness out of Lincoln, NE. We have been
-                    doing buisness
-                    for over 50 years. Started in 1964 by Merlyn Trotter, originally doing only floors, we have since
-                    expanded
-                    our company to a full service cleaning company. We provide impeccable cleaning from experience
-                    and knowledgable personal. We can clean many different types of projects from residental to
-                    commercial
-                    buildings, we have the equipment, experience and knowledge to handle almost any job. All at a fair
-                    and
-                    competetive
-                    rate. Just contact us and we will be happy to give a quote.
-                </p>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-      </main>
+      <>
+        <main className="content" id="about-content">
+          <nav className="link">
+            <Link to="/">Home</Link>
+            <Link to="apply">Apply</Link>
+          </nav>
+          <h1>Our Story</h1>
+          <p>Trotter Floor Cleaning Service is a small, family owned bussiness out of Lincoln, NE. We have been
+            doing buisness
+            for over 50 years. Started in 1964 by Merlyn Trotter, originally doing only floors, we have since
+            expanded
+            our company to a full service cleaning company. We provide impeccable cleaning from experience
+            and knowledgable personal. We can clean many different types of projects from residental to
+            commercial
+            buildings, we have the equipment, experience and knowledge to handle almost any job. All at a fair
+            and
+            competetive
+            rate. Just contact us and we will be happy to give a quote.
+          </p>
+        </main>
+      </>
     )
   }
+}
 
+export class Application extends React.Component {
+  render(){
+    return(
+      <main>
+      <nav>
+        <Link to = "/">Home</Link>
+        <Link to = "about">About</Link>
 
-
+      </nav>
+      <h1>Apply Today!</h1>
+      </main>
+    );
+  }
 }
