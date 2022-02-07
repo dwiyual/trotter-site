95b04305c8422449e81448defd5e8c234814f915
dwiyual <dwiyual@gmail.com>
2022-02-07 12:26:13 -0600

    add a new file to build a custom hook, also refactored appication component to functional compenent.

--- src/components/application.js
+++ src/components/application.js
@@ -1,78 +1,79 @@
 import { Button } from "reactstrap";
 import * as React from "react";
 import styled from "styled-components";
-import * as Yup from "yup";
+import { useState } from "react";
+//import * as Yup from "yup";
 
-const FormSchema = Yup.object().shape({
-  firstName: Yup
-      .string()
-      .require("Must have a first name."),
-  lastName: Yup
-      .string()
-      .required("Must have a last name.")
-})
+//const FormSchema = Yup.object().shape({
+//firstName: Yup
+//  .string()
+//  .require("Must have a first name."),
+// lastName: Yup
+//  .string()
+//  .required("Must have a last name.")
+//})
 
 const FormWrapper = styled.form`
 display: block;
 height: 20rem;
 margin: 1rem;
 `
-class Application extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      adress: "",
-      birthDate: "",
-      phoneNumber: ""
-    };
+function Application() {
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    adress: '',
+    birthDate: '',
+    phoneNumber: ''
+  });
+
+
+
+  const handleChange = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+    console.log(form)
   }
-      handleChange = Event => {
-        this.setState({...this.state, [Event.target.name]: Event.target.value})
-        console.log(this.state)
-      }
-      
-        handleSubmit = Event => {
-          Event.preventDefault();
-          alert("Your application has been sent!");
-          this.setState({...this.state,[Event.target.name]: ""});
-        }
-  render() {
-    return (
-      <>
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert("Your application has been sent!");
+    setForm({ ...form, [event.target.name]: "" });
+  }
+  return (
+    <>
       <h2>Apply Today</h2>
-        <FormWrapper>
-              <label>
-                First Name
-                <input onChange={this.handleChange} type="text" name="firstName" value={this.state.firstName} />
-                Last Name
-                <input onChange={this.handleChange} type="text" name="lastName" value={this.state.lastName} />
-              </label>
-              <br/>
-              <label>
-                 Adress
-                 <input onChange={this.handleChange} type="text" name="adress" value={this.state.adress} />
-              </label>
-              <label >
-                Email
-                  <input onChange={this.handleChange} typeof="email" name = " email" value = {this.state.email}/>
-              </label>
-              <label>
-              Date of Birth
-                <input onChange={this.handleChange} type="date" name="birthDate" value={this.state.birthDate}/>
-              </label>
-              <br/>
-              <label>
-                Phone Number
-                <input onChange={this.handleChange} type="tel" name="phoneNumber" value={this.state.phoneNumber} />
-              </label>
-        </FormWrapper>
-        <Button onClick={this.handleSubmit} color="primary">Submit</Button>
-      </>
-    );
-  }
+      <FormWrapper>
+        <label>
+          First Name
+          <input onChange={handleChange} type="text" name="firstName" value={form.firstName} />
+          Last Name
+          <input onChange={handleChange} type="text" name="lastName" value={form.lastName} />
+        </label>
+        <br />
+        <label>
+          Adress
+          <input onChange={handleChange} type="text" name="adress" value={form.adress} />
+        </label>
+        <label >
+          Email
+          <input onChange={handleChange} type="email" name=" email" value={form.email} />
+        </label>
+        <label>
+          Date of Birth
+          <input onChange={handleChange} type="date" name="birthDate" value={form.birthDate} />
+        </label>
+        <br />
+        <label>
+          Phone Number
+          <input onChange={handleChange} type="tel" name="phoneNumber" value={form.phoneNumber} />
+        </label>
+        <Button onClick={handleSubmit}>Submit</Button>
+      </FormWrapper>
+    </>
+   
+  );
 }
+
 
 export default Application
