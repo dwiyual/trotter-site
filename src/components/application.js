20e1c36bed4dbf61278f88f4e49e732155fc6a71
dwiyual <dwiyual@gmail.com>
2022-02-14 23:40:33 -0600

    set error function created to validate form and set error state, change handle function modified too, but still apply page does not render on browser although react says my app is compiled sucessfully.

--- src/components/application.js
+++ src/components/application.js
@@ -18,7 +18,7 @@ display: block;
 height: 20rem;
 margin: 1rem;
 `
-export function Application() {
+function Application() {
   const [form, setForm] = useState({
     firstName: '',
     lastName: '',
@@ -40,16 +40,17 @@ export function Application() {
 
   const [disabled, setDisabled] = useState(true);
 
-  useEffect(() => {
-
-    formSchema.isValid(form).then(valid => {
-      setDisabled(!valid);
-    });
-  }, [form]);
-
+  const setFormError= (name, value) => {
+    Yup.reach(formSchema, name).validate(value)
+      .then(() => setErrors({...errors, [name]: ''}))
+      .catche(err => setErrors({...errors, [name]: err.errors[0]}))
+  }
 
   const handleChange = e => {
-    setForm({ ...form, [e.target.name]: e.target.value })
+    const [name, type, value, checked ] = e.target;
+    const valueToUse = type === 'checkbox'? checked : value;
+    setFormError(name, valueToUse)
+    setForm({...form, [name]: value})
     console.log(form)
   }
 
@@ -64,66 +65,50 @@ export function Application() {
       birthDate: '',
       phoneNumber: ''
     });
+  }
 
-
-
-    Yup
-      .reach(formSchema, e.targe.name)
-      //we can then run validate using the value
-      .validate(e.target.value)
-      // if the validation is successful, we can clear the error message
-      .then(valid => {
-        setErrors({
-          ...errors, [e.target.name]: ""
-        });
-      })
-      // if the validation is unsuccessful, we can set the error message to the message
-      // returned from yup (that we created in our schema)
-      .catch(err => {
-        setErrors({
-          ...errors, [e.target.name]: err.errors[0]
-        });
-      });
-
-    // Whether or not our validation was successful, we will still set the state to the new value as the user is typing
-    setForm({
-      ...form, [e.target.name]: e.target.value
+  useEffect(() => {
+    formSchema.isValid(form).then(valid => {
+      setDisabled(!valid);
     });
-  };
+  }, [form]);
+
+  
+  
+
+ 
+  
   return (
-    <>
-      <h2>Apply Today</h2>
-      <FormWrapper>
-        <label>
-          First Name
-          <input onChange={handleChange()} type="text" name="firstName" value={form.firstName} />
-          Last Name
-          <input onChange={handleChange()} type="text" name="lastName" value={form.lastName} />
-        </label>
-        <br />
-        <label>
-          Adress
-          <input onChange={handleChange()} type="text" name="adress" value={form.adress} />
-        </label>
-        <label >
-          Email
-          <input onChange={handleChange()} type="email" name="email" value={form.email} />
-        </label>
-        <label>
-          Date of Birth
-          <input onChange={handleChange()} type="date" name="birthDate" value={form.birthDate} />
-        </label>
-        <br />
-        <label>
-          Phone Number
-          <input onChange={handleChange()} type="tel" name="phoneNumber" value={form.phoneNumber} />
-        </label>
-        <Button disabled={disabled} onSubmit={handleSubmit()} >Submit</Button>
-      </FormWrapper>
-    </>
-
-  );
-
+  <>
+    <h2>Apply Today</h2>
+    <FormWrapper>
+      <label>
+        First Name
+        <input onChange={handleChange()} type="text" name="firstName" value={form.firstName} />
+        Last Name
+        <input onChange={handleChange()} type="text" name="lastName" value={form.lastName} />
+      </label>
+      <br />
+      <label>
+        Adress
+        <input onChange={handleChange()} type="text" name="adress" value={form.adress} />
+      </label>
+      <label >
+        Email
+        <input onChange={handleChange()} type="email" name="email" value={form.email} />
+      </label>
+      <label>
+        Date of Birth
+        <input onChange={handleChange()} type="date" name="birthDate" value={form.birthDate} />
+      </label>
+      <br />
+      <label>
+        Phone Number
+        <input onChange={handleChange()} type="tel" name="phoneNumber" value={form.phoneNumber} />
+      </label>
+      <Button disabled={disabled} onSubmit={handleSubmit()} >Submit</Button>
+    </FormWrapper>
+  </>);
 }
 
 
